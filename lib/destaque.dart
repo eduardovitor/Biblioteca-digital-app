74fa66600c035c319ad0bfe37ccd9f71265beb4a
Eduardo Vitor <eduardovitor730@gmail.com>
2021-12-10 11:59:41 -0300

    Adicionada a barra de pesquisa de livros

--- lib/destaque.dart
+++ lib/destaque.dart
@@ -12,40 +12,40 @@ class Destaque extends StatefulWidget {
 class _DestaqueState extends State<Destaque> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const SizedBox(height: 20),
-      Padding(
-          padding: EdgeInsets.all(16),
-          child: Align(
-              alignment: Alignment.topLeft,
-              child: Text('Top escolhidos',
-                  style: GoogleFonts.carterOne(
-                      textStyle: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold))))),
-      const SizedBox(height: 40),
-      CarouselSlider(
-          items: [
-            InkWell(
-              child: Image.network(
-                  "https://images-na.ssl-images-amazon.com/images/I/71K0ACNXURL.jpg"),
-            ),
-            InkWell(
-                child: Image.network(
-                    'https://m.media-amazon.com/images/I/51nK9MO0G4L.jpg')),
-            InkWell(
-              child: Image.network(
-                  'https://images-na.ssl-images-amazon.com/images/I/51zoZfvbQhL._SX342_SY445_QL70_ML2_.jpg'),
-            )
-          ],
-          options: CarouselOptions(
-              height: 200,
-              autoPlay: true,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enableInfiniteScroll: false,
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              viewportFraction: 0.8))
-    ]);
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Top escolhidos',
+                style: GoogleFonts.carterOne(
+                    textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+            backgroundColor: const Color(0xFF61b98e),
+            automaticallyImplyLeading: false),
+        body: Column(children: [
+          const SizedBox(height: 20),
+          const SizedBox(height: 40),
+          CarouselSlider(
+              items: [
+                InkWell(
+                  child: Image.network(
+                      "https://images-na.ssl-images-amazon.com/images/I/71K0ACNXURL.jpg"),
+                ),
+                InkWell(
+                    child: Image.network(
+                        'https://m.media-amazon.com/images/I/51nK9MO0G4L.jpg')),
+                InkWell(
+                  child: Image.network(
+                      'https://images-na.ssl-images-amazon.com/images/I/51zoZfvbQhL._SX342_SY445_QL70_ML2_.jpg'),
+                )
+              ],
+              options: CarouselOptions(
+                  height: 200,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: false,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8))
+        ]));
   }
 }
--- lib/pesquisar.dart
+++ lib/pesquisar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Pesquisar extends StatefulWidget {
   const Pesquisar({Key? key}) : super(key: key);
@@ -8,8 +9,60 @@ class Pesquisar extends StatefulWidget {
 }
 
 class _PesquisarState extends State<Pesquisar> {
+  Icon customIcon = const Icon(Icons.search, color: Colors.white);
+  Widget customSearchBar = Text('Pesquisar livros',
+      style: GoogleFonts.carterOne(
+          textStyle: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)));
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: customSearchBar,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if (customIcon.icon == Icons.search) {
+                  customIcon = const Icon(Icons.cancel);
+                  customSearchBar = const ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    title: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Autores, livros ou ISBN',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                } else {
+                  customIcon = const Icon(Icons.search);
+                  customSearchBar = Text('Pesquisar livros',
+                      style: GoogleFonts.carterOne(
+                          textStyle: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)));
+                }
+              });
+            },
+            icon: customIcon,
+          )
+        ],
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xFF61b98e),
+      ),
+    );
   }
 }
